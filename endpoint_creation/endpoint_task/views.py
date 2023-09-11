c17527a467bd49114a825b51b81b97eb41d04402
Afolabi Asogba <apholy2010@gmail.com>
2023-09-11 01:07:32 +0100

    first commit

---
+++ endpoint_creation/endpoint_task/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+
+# Create your views here.
+from django.http import JsonResponse
+from django.utils import timezone
+
+def endpoint_view(request):
+    slack_name = request.GET.get('slack_name', 'apholyrender')
+    track = request.GET.get('track', 'backend')
+    current_day = timezone.now().strftime('%A')
+    utc_time = timezone.now().strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    # Replace with your GitHub file and repository URLs
+    github_file_url = 'https://github.com/username/repo/blob/main/file_name.ext'
+    github_repo_url = 'https://github.com/username/repo'
+
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
+    }
+
+    return JsonResponse(response_data)
