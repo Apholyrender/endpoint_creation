27ef5ea2084b77ebf211950e3a813a49e5ec5c4d
Afolabi Asogba <apholy2010@gmail.com>
2023-09-11 02:11:00 +0100

    generated a requirements.txt for hosting

--- endpoint_creation/endpoint_creation/settings.py
+++ endpoint_creation/endpoint_creation/settings.py
@@ -121,3 +121,15 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+
+import os
+
+# ...
+
+# Configure static and media files
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
