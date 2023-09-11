f95501c44ff40fc66a8392233eee506ab1d04b92
Afolabi Asogba <apholy2010@gmail.com>
2023-09-11 01:53:30 +0100

    made some corrections

--- endpoint_creation/endpoint_creation/urls.py
+++ endpoint_creation/endpoint_creation/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('endpoint_app.urls')),
+    path('', include('endpoint_task.urls')),
 ]
